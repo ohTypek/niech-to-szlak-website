8b319b17d557f60fcb2084abf6cbf8b393c5879c
ohTypek <toptypek@wp.pl>
2022-08-22 10:55:05 +0200

    added needed files and made a design for the website

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = function odliczanie() {
+
+    var czas = new Date();
+
+    var godzina = czas.getHours();
+    var minuta = czas.getMinutes();
+    var sekunda = czas.getSeconds();
+
+    if(sekunda < 10) sekunda = "0"+sekunda;
+    if(minuta < 10) minuta = "0"+minuta;
+
+    document.getElementById("p").innerHTML = godzina + ":" + minuta + ":" + sekunda;
+    setTimeout("odliczanie()", 1000);
+}
